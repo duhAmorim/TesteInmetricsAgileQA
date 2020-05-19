982cc1f78fa541b527d149222443c70ea002c881
Eduardo Amorim <duh-amorim@hotmail.com>
2020-05-18 22:52:15 -0300

    hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,4 @@
-require 'report_builder'
+
 After do |scenario|
   scenario_name = scenario.name.gsub(/\s+/,'_').tr('/','_')
 
@@ -10,15 +10,4 @@ After do |scenario|
   
 end
 
-at_exit do
-  
-ReportBuilder.input_path = "report.json"
-ReportBuilder.configure do |config|
-   config.report_path = "reports/run"
-   config.report_types = [:json, :html]
- options = {
-   report_title: "Automacao QA"
- }
-ReportBuilder.build_report options
-end
-end
+
